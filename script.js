7f3c86fb4b2793f1d9f4536fbfef326a4d0059ee
silc-o <bhussienaldondie@gmail.com>
2026-07-07 19:00:21 +0800

    Implement book card display and update book model in library

--- script.js
+++ script.js
@@ -1,16 +1,83 @@
 const myLibrary = [];
 
-
-function Book(title, author, pages, currentProgress, description) {
+function Book(title, author, bookPage, currentPage, description) {
   this.bookID = crypto.randomUUID();
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.currentProgress = currentProgress;
+  this.bookPage = bookPage;
+  this.currentPage = currentPage;
   this.description = description;
 }
 
-function addBookToLibrary(title, author, pages, currentProgress, description) {
-  const newBook = new Book(title, author, pages, currentProgress, description)
+function addBookToLibrary(title, author, bookPage, currentPage, description) {
+  const newBook = new Book(title, author, bookPage, currentPage, description)
   myLibrary.push(newBook);
+}
+
+function createBook(index) {
+  const libraryContainer = document.querySelector("#library-container");
+
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("book-card");
+
+  const bookDetails = document.createElement("div");
+  bookDetails.classList.add("book-details")
+  const title = document.createElement("h3");
+  title.textContent = myLibrary[index].title;
+  const author = document.createElement("em");
+  author.textContent = myLibrary[index].author;
+  const description = document.createElement("div");
+  description.textContent = myLibrary[index].description;
+
+  bookDetails.appendChild(title);
+  bookDetails.appendChild(author);
+  bookDetails.appendChild(description);
+
+  const bookProgress = document.createElement("div");
+  bookProgress.classList.add("book-progress");
+  const lineOne = document.createElement("p");
+  lineOne.textContent = "Page"
+  const currentPage = document.createElement("p");
+  currentPage.classList.add("current-page");
+  currentPage.textContent = myLibrary[index].currentPage;
+  const lineThree = document.createElement("p");
+  lineThree.textContent = "of"
+  const bookPage = document.createElement("p");
+  bookPage.classList.add("bookPage");
+  bookPage.textContent = myLibrary[index].bookPage;
+
+  bookProgress.appendChild(lineOne);
+  bookProgress.appendChild(currentPage);
+  bookProgress.appendChild(lineThree);
+  bookProgress.appendChild(bookPage);
+
+  const bookButtons = document.createElement("div");
+  bookButtons.classList.add("book-buttons");
+  
+  const editButton = document.createElement("button");
+  const deleteButton = document.createElement("button");
+  editButton.classList.add("btn", "edit");
+  deleteButton.classList.add("btn", "delete");
+  editButton.textContent = "Edit";
+  deleteButton.textContent = "Delete";
+
+  bookButtons.appendChild(editButton);
+  bookButtons.appendChild(deleteButton);
+
+  bookCard.appendChild(bookDetails);
+  bookCard.appendChild(bookProgress);
+  bookCard.appendChild(bookButtons);
+
+  libraryContainer.appendChild(bookCard);
+}
+
+function displayBooks() {
+  const container = document.querySelector("#library-container");
+  container.innerHTML = "";
+
+  const librarySize = myLibrary.length;
+
+  for (let i = 0; i < librarySize; i++) {
+    createBook(i);
+  }
 }
